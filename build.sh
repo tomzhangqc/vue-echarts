6f59cb12d23963301f48055bf4b48d038f4357b8
zhangqingchun4 <zhangqingchun4@xdf.cn>
2020-11-13 15:25:48 +0800

    添加build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+image_name=$1
+if [ -z $image_name ]
+then
+echo "image_name is null"
+exit 1
+fi
+date=`date +%Y%m%d%H%M%S`
+docker build -t registry.cn-hangzhou.aliyuncs.com/ceres-spring/$image_name:$date .
+if [ $? -eq 0 ]
+then
+echo "build success"
+docker push registry.cn-hangzhou.aliyuncs.com/ceres-spring/$image_name:$date
+if [ $? -eq 0 ]
+then
+echo "push success"
+docker rmi registry.cn-hangzhou.aliyuncs.com/ceres-spring/$image_name:$date
+kubectl set image deployment/$image_name $image_name=registry.cn-hangzhou.aliyuncs.com/ceres-spring/$image_name:$date -n spring-cloud
+else
+echo "push error"
+fi
+else
+echo "build error"
+fi
